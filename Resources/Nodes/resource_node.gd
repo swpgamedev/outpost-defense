02b241393452360589a46c060ef58d6271aa2892
SwPGameDev <swpgamedev@gmail.com>
2026-05-22 00:03:34 -0600

    Resource Node class

---
+++ Resources/Nodes/resource_node.gd
@@ -0,0 +1,7 @@
+extends StaticBody3D
+class_name ResourceNode
+
+enum ResourceType {None, Gold, Wood, Stone, Iron, Crystal}
+@export var node_resource : ResourceType
+
+@export var chunks_avaliable : int
